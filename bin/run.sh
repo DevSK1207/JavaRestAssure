df844ae07bef9e81378d802f97f2e321c60baaa0
nowrin1205 <khairun.nahar@daraz.edu.bd>
2023-01-23 15:33:55 +0600

    ➕ Add run shell run to execute text case
    Type(s): ADD
    
    Issue(s):
    - JIRA: 454

---
+++ bin/run.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+
+# Fetch the latest of running container
+docker ps -l
+
+# The docker rm command is used to remove the container. The $(...) syntax is used to execute the docker ps -l -q command and pass the output (which is the container ID of the latest container) as an argument to the docker rm command.
+#This approach will delete the latest running container
+docker rm $(docker ps -l -q)
+
+#To remove a Docker image forcefully
+docker rmi -f java-automation
+
+# Build the Docker image
+docker build -t java-automation .
+
+# Run a new container based on the image, and Run test case command in container
+docker run -it java-automation ./gradlew test
+
+
+
+
+#run all test suit
+#./gradlew test
+
+#Run specific test case
+#./gradlew test --tests APITest
+
+#to verify shell file RUN -> bin/chmod u+x run.sh
